ee596f2938844aaede3d9eae3e1346c968365f48
odiliemmanuel <odiliejeh09@gmail.com>
2026-04-26 15:39:49 +0100

    "estate"

---
+++ model/Visitor.go
@@ -0,0 +1,11 @@
+package model
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Visitor struct {
+	ID             primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Name           string             `bson:"name"`
+	PhoneNumber    string             `bson:"phoneNumber"`
+	PurposeOfVisit string             `bson:"purposeOfVisit"`
+	ResidentId     string             `bson:"residentId"`
+}
