194b247de57936010e52c7341b15984cbaef1123
odiliemmanuel <odiliejeh09@gmail.com>
2026-04-26 16:20:52 +0100

    "estate"

--- repositories/GatePassRepository.go
+++ repositories/GatePassRepository.go
@@ -1,1 +1,10 @@
 package repositories
+
+import "RealEstate/models"
+
+type GatePassRepository interface {
+	Save(gatePass models.GatePass) (models.GatePass, error)
+	FindAll() ([]models.GatePass, error)
+	FindById(gatePassId string) (models.GatePass, error)
+	FindByResidentId(residentId string) (models.GatePass, error)
+}
