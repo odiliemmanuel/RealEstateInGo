0ccc6af936a0623d3be7033e35c123a77cb213c0
odiliemmanuel <odiliejeh09@gmail.com>
2026-04-26 18:14:09 +0100

    "estate"

--- services/ResidentManagementService.go
+++ services/ResidentManagementService.go
@@ -1,13 +1,35 @@
 package services
 
 import (
-	"RealEstate/models"
+	"RealEstate/dtos/requests"
+	"RealEstate/dtos/responses"
 	"RealEstate/repositories"
+	"RealEstate/utils"
+	"errors"
 )
 
-type ResidentManagementService interface {
-	OnboardResident()
+type ResidentService struct {
+	residentRepository repositories.ResidentRepository
 }
 
-type ResidentManagementServiceImpl struct {
+func NewResidentManagementService(r repositories.ResidentRepository) *ResidentService {
+	return &ResidentService{residentRepository: r}
+}
+
+func (rs *ResidentService) OnboardNewResident(request requests.OnboardResidentRequest) (responses.OnboardResidentResponse, error) {
+	_, err := rs.residentRepository.FindByEmail(request.Email)
+
+	if err == nil {
+		return responses.OnboardResidentResponse{}, errors.New("resident already exists")
+	}
+
+	resident := utils.MapOnboardResidentRequestToResident(request)
+
+	savedUser, err := rs.residentRepository.Save(resident)
+	if err != nil {
+		return responses.OnboardResidentResponse{}, err
+	}
+
+	return utils.MapResidentToOnboardResidentResponse(savedUser), nil
+
 }
