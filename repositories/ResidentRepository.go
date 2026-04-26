fcb9cc4dad8b8b08065da595807827ba5cd2de54
odiliemmanuel <odiliejeh09@gmail.com>
2026-04-26 16:20:59 +0100

    "estate"

--- repositories/ResidentRepository.go
+++ repositories/ResidentRepository.go
@@ -1,1 +1,90 @@
 package repositories
+
+import (
+	"RealEstate/config"
+	"RealEstate/models"
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type ResidentRepository interface {
+	Save(entry models.Resident) (models.Resident, error)
+	FindById(residentId string) (models.Resident, error)
+	FindByEmail(email string) (models.Resident, error)
+	FindAll() ([]models.Resident, error)
+}
+type residentRepository struct {
+	collection *mongo.Collection
+}
+
+func NewResidentRepository() ResidentRepository {
+	return &residentRepository{
+		collection: config.DB.Collection("users"),
+	}
+}
+
+func (r residentRepository) Save(resident models.Resident) (models.Resident, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	result, err := r.collection.InsertOne(ctx, resident)
+	if err != nil {
+		return models.Resident{}, err
+	}
+
+	resident.ID = result.InsertedID.(primitive.ObjectID)
+
+	return resident, nil
+}
+
+func (r residentRepository) FindById(residentId string) (models.Resident, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	objID, err := primitive.ObjectIDFromHex(residentId)
+	if err != nil {
+		return models.Resident{}, err
+	}
+
+	var resident models.Resident
+
+	err = r.collection.FindOne(ctx, bson.M{"_id": objID}).Decode(&resident)
+	if err != nil {
+		return models.Resident{}, err
+	}
+
+	return resident, nil
+}
+
+func (r residentRepository) FindByEmail(email string) (models.Resident, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	var resident models.Resident
+	err := r.collection.FindOne(ctx, bson.M{"email": email}).Decode(&resident)
+	if err != nil {
+		return models.Resident{}, err
+	}
+
+	return resident, nil
+}
+
+func (r residentRepository) FindAll() ([]models.Resident, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	cursor, err := r.collection.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+
+	var residents []models.Resident
+	if err := cursor.All(ctx, &residents); err != nil {
+		return nil, err
+	}
+	return residents, nil
+}
